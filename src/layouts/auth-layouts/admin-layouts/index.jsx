ed1ae4b66fd0c52a18c72199a0639f81596915ba
namnd <namnd@rabiloo.com>
2023-11-13 16:52:05 +0700

    fix ui

--- src/layouts/auth-layouts/admin-layouts/index.jsx
+++ src/layouts/auth-layouts/admin-layouts/index.jsx
@@ -4,10 +4,12 @@ import PropTypes from "prop-types";
 
 const AdminAuthLayout = ({ title = "Form đăng nhập quản trị" }) => {
   return (
-    <div className="px-5 lg:px-0 flex justify-center items-center min-h-screen max-w-[1000px] mx-auto">
-      <Card title={title} className="w-full shadow-md ">
-        <Outlet />
-      </Card>
+    <div className="bg-gradient-to-b from-gray-900 to-gray-600 bg-gradient-to-r">
+      <div className="px-5 lg:px-0 flex justify-center items-center min-h-screen max-w-[1000px] mx-auto">
+        <Card title={title} className="w-full shadow-md ">
+          <Outlet />
+        </Card>
+      </div>
     </div>
   );
 };
--- src/layouts/auth-layouts/user-layouts/index.jsx
+++ src/layouts/auth-layouts/user-layouts/index.jsx
@@ -3,10 +3,12 @@ import { Outlet } from "react-router-dom";
 
 const UserAuthLayout = () => {
   return (
-    <div className="px-5 lg:px-0 flex justify-center items-center min-h-screen max-w-[1000px] mx-auto">
-      <Card className="w-full shadow-md">
-        <Outlet />
-      </Card>
+    <div className="bg-gradient-to-b from-gray-900 to-gray-600 bg-gradient-to-r">
+      <div className="px-5 lg:px-0 flex justify-center items-center min-h-screen max-w-[1000px] mx-auto">
+        <Card className="w-full shadow-md">
+          <Outlet />
+        </Card>
+      </div>
     </div>
   );
 };
