8c2cedbf057384504c854bddbee47f31e303576e
nam <namsupermen06@gmail.com>
2023-12-12 00:45:19 +0700

    fix ui edit post, handle update photo post for admin

--- src/components/PostContent.jsx
+++ src/components/PostContent.jsx
@@ -8,35 +8,36 @@ const PostContent = ({ post }) => {
   return (
     <Flex vertical gap="large">
       <Flex
-        vertical
-        align="flex-start"
+        justify="space-between"
         gap="small"
-        className="w-full bg-slate-100 px-8 py-5"
+        className="relative flex-row-reverse w-full px-8 py-5 bg-slate-100"
       >
-        <img src={post?.photo} alt="" className="w-full h-[100px]" />
-        <h1 className="capitalize">{post?.title}</h1>
-        <Flex gap="small" align="center">
-          <Avatar>
-            {post?.created_by_admin?.username &&
-              customRenderAvatar(post?.created_by_admin?.username)}
-          </Avatar>
-          <span>by: {post?.created_by_admin?.username}</span>
-          <span>on: {post?.created_at && customRenderDate(post?.created_at)}</span>
+        <img src={post?.photo} alt="" className="w-[200px] h-[200px] object-cover" />
+        <Flex vertical gap="small">
+          <h1 className="capitalize">{post?.title}</h1>
+          <Flex gap="small" align="center">
+            <Avatar>
+              {post?.created_by_admin?.username &&
+                customRenderAvatar(post?.created_by_admin?.username)}
+            </Avatar>
+            <span>by: {post?.created_by_admin?.username}</span>
+            <span>on: {post?.created_at && customRenderDate(post?.created_at)}</span>
+          </Flex>
+          <Flex gap="small" align="center" className="text-lg">
+            <EyeOutlined />
+            <span>{post?.view}</span>
+          </Flex>
+          <div>
+            Danh mục:
+            <Tag>{post?.category?.name}</Tag>
+          </div>
+          <div>
+            Thẻ:
+            {post?.tags_info?.map((tag) => (
+              <Tag key={tag?.id}>{tag?.name}</Tag>
+            ))}
+          </div>
         </Flex>
-        <Flex gap="small" align="center" className="text-lg">
-          <EyeOutlined />
-          <span>{post?.view}</span>
-        </Flex>
-        <div>
-          Danh mục:
-          <Tag>{post?.category?.name}</Tag>
-        </div>
-        <div>
-          Thẻ:
-          {post?.tags_info?.map((tag) => (
-            <Tag key={tag?.id}>{tag?.name}</Tag>
-          ))}
-        </div>
       </Flex>
       <div
         className="w-full h-fit px-8 py-5 bg-slate-100 text-justify text-[16px] leading-relaxed"
